e0109775f3f26d9787f9d48331de9c785aef6f6e
copaiement <cole.paiement@gmail.com>
2023-02-22 18:46:32 -0800

    multiple updates to style and js code.

--- js-calculator.js
+++ js-calculator.js
@@ -1,13 +1,67 @@
 // script for JS Calculator
 
-//testing script
+//setup
+const output = document.querySelector(".display-output");
+let num1 = "";
+let num2 = "";
+let ans = 0;
+let operator;
 
-const num1Input = document.getElementById("num1");
-const num2Input = document.getElementById("num2");
-const numsPrint = document.getElementById("nums");
-const output = document.getElementById("result");
-let num1;
-let num2;
+//wait for on button to be pressed
+const on = document.getElementById("btn-on");
+on.addEventListener("click", () => {
+    //get listeners for number buttons
+    const numBtns = document.querySelectorAll(".number-btn");
+    numBtns.forEach(btn => {
+        btn.addEventListener("click", getNum);
+    });
+    //get listeners for operations buttons
+    const operBtns = document.querySelectorAll(".oper-btn");
+    operBtns.forEach(btn => {
+        btn.addEventListener("click", getOper);
+    });
+    //update display class to .active
+    const display = document.querySelector(".display");
+    display.classList.add("active");
+    //display 0
+    output.textContent = 0;
+}, {once: true});
+
+
+
+//get number
+function getNum(e) {
+    //if operator is blank we're still on num1
+    if (operator === "")
+    num1 += e.target.innerText;
+    output.textContent = num1;
+}
+//get operator
+function getOper(e) {
+    //if operator is hit while num1 is blank, we're using ans as num1 (initially 0)
+    
+    switch(e.target.innerText) {
+        case "CLR":
+            clear();
+            break;
+
+    }
+}
+
+//operate function
+
+function operate(operator, num1, num2) {
+
+}
+
+//clear function
+function clear() {
+    num1 = "";
+    num2 = "";
+    ans = 0;
+    operator = "";
+    output.textContent = 0;
+}
 
 //math functions
 
@@ -35,10 +89,6 @@ function divide() {
     return result;
 }
 
-//operate function
 
-function operate(operator, num1, num2) {
-
-}
 
 
